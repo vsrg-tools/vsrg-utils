a9f85d6732cdb6068d38a4e6a806865936447ab8
masked <37945842+Maskeddd@users.noreply.github.com>
2022-12-27 00:57:58 +1000

    feat: implement to_lanes

--- src/replays/replay_key_press_state.rs
+++ src/replays/replay_key_press_state.rs
@@ -1,5 +1,21 @@
 use bitflags::bitflags;
 
+macro_rules! bitmask_to_vec {
+    ($inst:expr, $($bit:expr => $repr:expr),*) => {
+        {
+            let mut lanes: Vec<i32> = Vec::new();
+
+            $(
+                if $inst.contains($bit) {
+                    lanes.push($repr);
+                }
+            )*
+
+            lanes
+        }
+    };
+}
+
 bitflags! {
     pub struct ReplayKeyPressState: i16 {
         const K1 = 1 << 0;
@@ -11,5 +27,22 @@ bitflags! {
         const K7 = 1 << 6;
         const K8 = 1 << 7;
         const K9 = 1 << 8;
+    }
+}
+
+impl ReplayKeyPressState {
+    pub fn to_lanes(&self) -> Vec<i32> {
+        bitmask_to_vec!(
+            self,
+            ReplayKeyPressState::K1 => 0,
+            ReplayKeyPressState::K2 => 1,
+            ReplayKeyPressState::K3 => 2,
+            ReplayKeyPressState::K4 => 3,
+            ReplayKeyPressState::K5 => 4,
+            ReplayKeyPressState::K6 => 5,
+            ReplayKeyPressState::K7 => 6,
+            ReplayKeyPressState::K8 => 7,
+            ReplayKeyPressState::K9 => 8
+        )
     }
 }
