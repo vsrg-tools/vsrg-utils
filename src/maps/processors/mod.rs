087181085ab535f8193fc453ab9d58128019cbc5
Norizon <norizontunes@gmail.com>
2022-12-29 19:10:28 +0700

    Oopsies

--- src/maps/processors/mod.rs
+++ src/maps/processors/mod.rs
@@ -2,7 +2,9 @@ pub mod structures;
 
 mod difficulty_processor;
 mod rating_processor;
+mod scoring;
 mod strain_constants;
 pub use difficulty_processor::*;
 pub use rating_processor::*;
+pub use scoring::*;
 pub use strain_constants::*;
--- src/maps/processors/structures/mod.rs
+++ src/maps/processors/structures/mod.rs
@@ -2,7 +2,6 @@ mod finger_action;
 mod finger_state;
 mod hand;
 mod ln_layer_type;
-mod scoring;
 mod strain_solver_data;
 mod strain_solver_hit_object;
 
@@ -10,6 +9,5 @@ pub use finger_action::*;
 pub use finger_state::*;
 pub use hand::*;
 pub use ln_layer_type::*;
-pub use scoring::*;
 pub use strain_solver_data::*;
 pub use strain_solver_hit_object::*;
