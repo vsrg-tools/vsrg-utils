54f610cf50c3463816b6479cd1816d1025330611
masked <37945842+Maskeddd@users.noreply.github.com>
2023-01-21 18:36:06 +1000

    feat: load audio file from qp

--- src/maps/qp.rs
+++ src/maps/qp.rs
@@ -1,33 +1,41 @@
 use crate::maps::QuaverMap;
+use anyhow::Result;
 use std::{collections::HashMap, io::Read};
 use zip;
 
 #[derive(Debug)]
 pub struct QuaverMapset {
     pub maps: HashMap<String, QuaverMap>,
+    pub audio: Vec<u8>,
 }
 
 impl QuaverMapset {
-    pub fn from_path(path: &str) -> Self {
+    pub fn from_path(path: &str) -> Result<Self> {
         let mut self_ = Self {
             maps: HashMap::new(),
+            audio: Vec::new(),
         };
 
-        let qp_file = std::fs::File::open(path).unwrap();
+        let qp_file = std::fs::File::open(path)?;
 
-        let zip = zip::ZipArchive::new(&qp_file).unwrap();
+        let zip = zip::ZipArchive::new(&qp_file)?;
 
         for name in zip.file_names() {
+            let mut zip = zip.clone();
             if name.contains(".qua") {
                 let mut contents = String::new();
-                let mut zip = zip::ZipArchive::new(&qp_file).unwrap();
-                let mut qua_file = zip.by_name(name).unwrap();
-                qua_file.read_to_string(&mut contents).unwrap();
+                let mut qua_file = zip.by_name(name)?;
+                qua_file.read_to_string(&mut contents)?;
                 let md5 = format!("{:x}", md5::compute(&contents));
                 self_.maps.insert(md5, QuaverMap::from_string(&contents));
+            } else if name.contains(".mp3") {
+                let mut audio_file = zip.by_name(name)?;
+                let mut buffer: Vec<u8> = Vec::new();
+                audio_file.read_to_end(&mut buffer)?;
+                self_.audio = buffer
             }
         }
 
-        self_
+        Ok(self_)
     }
 }
